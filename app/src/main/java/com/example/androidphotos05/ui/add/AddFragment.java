e7fc9bcf534dc0e6b2cc5f8ab0a2fd9f66bccbcd
thanna96 <thanna96@gmail.com>
2020-04-26 21:58:32 -0400

    Changed the Theme and added in some GUI to the "add photos" fragment. Also managed to select a photo from the phones gallery.

--- app/src/main/java/com/example/androidphotos05/ui/add/AddFragment.java
+++ app/src/main/java/com/example/androidphotos05/ui/add/AddFragment.java
@@ -1,9 +1,16 @@
 package com.example.androidphotos05.ui.add;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,14 +21,13 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.androidphotos05.R;
 
-public class AddFragment extends Fragment {
-
-    private AddViewModel addViewModel;
+public class AddFragment extends Fragment implements View.OnClickListener {
+    private static final int SELECT_PICTURE = 1;
+    private ImageView img;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        addViewModel =
-                ViewModelProviders.of(this).get(AddViewModel.class);
+        AddViewModel addViewModel = ViewModelProviders.of(this).get(AddViewModel.class);
         View root = inflater.inflate(R.layout.fragment_add, container, false);
         final TextView textView = root.findViewById(R.id.text_add);
         addViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -30,6 +36,29 @@ public class AddFragment extends Fragment {
                 textView.setText(s);
             }
         });
+        Button addButton = root.findViewById(R.id.add_button);
+        addButton.setOnClickListener(this);
+        img = root.findViewById(R.id.selected_photo);
         return root;
     }
+
+    @Override
+    public void onClick(View v) {
+        // implements your things
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                img.setImageURI(selectedImageUri);
+            }
+        }
+    }
+
+
 }
