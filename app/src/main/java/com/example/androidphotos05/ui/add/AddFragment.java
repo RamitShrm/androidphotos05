392a97f71c9a0edfc3f12370c0873adae4f0bf22
thanna96 <thanna96@gmail.com>
2020-04-28 21:05:27 -0400

    Started working on Search UI

--- app/src/main/java/com/example/androidphotos05/ui/add/AddFragment.java
+++ app/src/main/java/com/example/androidphotos05/ui/add/AddFragment.java
@@ -1,51 +1,35 @@
 package com.example.androidphotos05.ui.add;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import com.example.androidphotos05.Photo;
 import com.example.androidphotos05.PhotoActivity;
 import com.example.androidphotos05.R;
 
-import java.util.Objects;
-
 public class AddFragment extends Fragment implements View.OnClickListener {
     private static final int SELECT_PICTURE = 1;
     private ImageView img;
-    String picturePath;
+    private String picturePath;
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AddViewModel addViewModel = ViewModelProviders.of(this).get(AddViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_add, container, false);
-        addViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
 
-            }
-        });
+        View root = inflater.inflate(R.layout.fragment_add, container, false);
+
         img = root.findViewById(R.id.selected_photo);
         Button addButton = root.findViewById(R.id.select_button);
         addButton.setOnClickListener(this);
-
-        Button button = (Button) root.findViewById(R.id.add_photo);
+        Button button = root.findViewById(R.id.add_photo);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -71,6 +55,7 @@ public class AddFragment extends Fragment implements View.OnClickListener {
         if (resultCode == Activity.RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 Uri selectedImageUri = data.getData();
+                assert selectedImageUri != null;
                 picturePath = selectedImageUri.toString();
                 img.setImageURI(selectedImageUri);
             }
--- app/src/main/java/com/example/androidphotos05/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/androidphotos05/ui/search/SearchFragment.java
@@ -16,20 +16,19 @@ import com.example.androidphotos05.R;
 
 public class SearchFragment extends Fragment {
 
-    private SearchViewModel searchViewModel;
+    //private SearchViewModel searchViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        searchViewModel =
-                ViewModelProviders.of(this).get(SearchViewModel.class);
+        //searchViewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_search);
-        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        //final TextView textView = root.findViewById(R.id.text_search);
+        /*searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
 }
